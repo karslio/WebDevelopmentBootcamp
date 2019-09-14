6c1e1ec9df091476d6cc90363649f32d3a390a41
karslio <karslio@users.noreply.github.com>
2019-09-15 00:53:55 +0200

    updated

---
+++ 20_2_Newsletter-Signup/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  var firstName = req.body.fname;
+  var lastName = req.body.lname;
+  var email = req.body.email;
+  console.log(req.body, firstName, lastName, email);
+});
+app.listen(3002, function() {
+  console.log("Server is running on port 3000");
+});
