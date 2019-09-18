653898bba9965471c632481a717f433e0f641ffd
karslio <karslio@users.noreply.github.com>
2019-09-18 13:22:14 +0200

    21 todolist finish

--- 21_EJS_todolis-v1/app.js
+++ 21_EJS_todolis-v1/app.js
@@ -1,42 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const items = ["Buy Food", "cook food", "eat food"];
+const workItems = ["oguzhan"];
 
 app.get("/", function(req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  const day = date.getDay();
+  res.render("list", { listTitle: day, newListItems: items });
+});
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 0:
-      day = "Wednesday";
-      break;
-    case 0:
-      day = "Thursday";
-      break;
-    case 0:
-      day = "Friday";
-      break;
-    case 0:
-      day = "Saturday";
-      break;
-
-    default:
-      console.log(`error: current day is equel ${currentDay} `);
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
   }
-  res.render("list", { kindOfDay: day });
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function(req, res) {
