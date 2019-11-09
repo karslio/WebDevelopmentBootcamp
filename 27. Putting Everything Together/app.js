a6af64cda27717accf2298c955ef2c02ac16cee6
Your Namekarslio <karslionl@gmail.com>
2019-11-09 10:36:58 +0100

    todolist

--- 27. Putting Everything Together/app.js
+++ 27. Putting Everything Together/app.js
@@ -1,13 +1,10 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
-
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -16,23 +13,13 @@ mongoose.connect("mongodb://localhost:27017/todolistDB", {
   useUnifiedTopology: true
 });
 
-const itemsSchema = {
-  name: String
-};
+const itemsSchema = { name: String };
 
 const Item = mongoose.model("Item", itemsSchema);
 
-const item1 = new Item({
-  name: "Welcome to your todoList!"
-});
-
-const item2 = new Item({
-  name: "hit the + button to aff a new item"
-});
-
-const item3 = new Item({
-  name: "<-- Hit this to delete an item."
-});
+const item1 = new Item({ name: "Welcome to your todoList!" });
+const item2 = new Item({ name: "hit the + button to aff a new item" });
+const item3 = new Item({ name: "<-- Hit this to delete an item." });
 
 const defaultsItems = [item1, item2, item3];
 
@@ -62,25 +49,47 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
   const itemName = req.body.newItem;
-  const item = new Item({
-    name: itemName
-  });
-  item.save();
-  res.redirect("/");
+  const listName = req.body.list;
+  const item = new Item({ name: itemName });
+
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, function(err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
 });
 
 app.post("/delete", function(req, res) {
   const checkedItem = req.body.checkbox;
-  Item.findByIdAndRemove(checkedItem, function(err) {
-    if (!err) {
-      console.log("Succesfully deleted checkedItem from todolistDB ");
-    }
-  });
-  res.redirect("/");
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItem, function(err) {
+      if (!err) {
+        console.log("Succesfully deleted checkedItem from todolistDB ");
+      }
+    });
+    res.redirect("/");
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItem } } },
+      function(err, foundList) {
+        if (!err) {
+          res.redirect("/" + listName);
+        }
+      }
+    );
+  }
 });
 
 app.get("/:customListName", function(req, res) {
-  customListName = req.params.customListName;
+  customListName = _.capitalize(req.params.customListName);
   List.findOne({ name: customListName }, function(err, foundList) {
     if (!err) {
       if (!foundList) {
@@ -89,6 +98,7 @@ app.get("/:customListName", function(req, res) {
           items: defaultsItems
         });
         list.save();
+        res.redirect("/" + customListName);
       } else {
         res.render("list", {
           listTitle: foundList.name,
@@ -97,10 +107,6 @@ app.get("/:customListName", function(req, res) {
       }
     }
   });
-});
-
-app.get("/about", function(req, res) {
-  res.render("about");
 });
 
 app.listen(3000, function() {
